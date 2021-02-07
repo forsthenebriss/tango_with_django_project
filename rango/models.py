4b185a93e34aedecff06826ea1a80c3257a720d8
Romana Canigova <2481149c@student.gla.ac.uk>
2021-02-07 22:53:32 +0000

    chpt5

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    #a name string of maximum 128 chars that must not already exist
+    name = models.CharField(max_length=128, unique=True)
+    #number of likes and views
+    likes = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+    
+   #return name as string for easier manipulation
+    def __str__(self):
+        return self.name
+    
+    #changes the typo 'categorys' in plural to 'categories'
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+class Page(models.Model):
+    #if deleted, everything connected deleted with the category
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    #a title string of maximum 128 chars
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+    
+    
+    #return name as string for easier manipulation
+    def __str__(self):
+        return self.title
